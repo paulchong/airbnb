c106b304c6eb15e923ee39f99973a4b3107040cd
Paul <paulchong.nz@gmail.com>
2013-12-25 15:48:46 -0800

    first commit for airbnb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  # before_filter :redirect_to_reservation, :only => [:index]
+
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Htabnb::Application.config.secret_key_base = 'd1beec8d0725bd7ce45ff9729b8f9b3427c0434a729a170ffe2602da63183fbdae010e6050d110b618572718c8d8d022ccc9da4c71278bc4b103471f74380165'
